7f22be3518306bde6fb9f59917b9b4119e77026f
yexiaodong <2283138757@qq.com>
2019-04-18 15:28:41 +0800

    doUI

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+// 引入插件
+import doUI from './ui'
+// 使用插件
+Vue.use(doUI);
 
 new Vue({
   router,
---
+++ src/ui/header/src/header.vue
@@ -0,0 +1,12 @@
+<template>    
+    <div class="" >        
+        组件测试heaer
+    </div>
+</template>
+
+<<script>
+export default {
+    name:'do-header',
+    
+}
+</script>
